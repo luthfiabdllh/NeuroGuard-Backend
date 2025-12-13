1566d9218afa0d3e9de6fbf8fce9f7f8071c1c83
Ahmad Luthfi Abdillah <ahmadluthfiabdillah@mail.ugm.ac.id>
2025-12-13 20:44:17 +0700

    feat: Implement article CRUD operations using Prisma

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,26 +1,41 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
 
 @Injectable()
 export class ArticlesService {
+    constructor(private prisma: PrismaService) { }
+
     create(createArticleDto: CreateArticleDto) {
-        return 'This action adds a new article';
+        return this.prisma.article.create({
+            data: createArticleDto,
+        });
     }
 
     findAll() {
-        return `This action returns all articles`;
+        return this.prisma.article.findMany({
+            include: { category: true },
+        });
     }
 
     findOne(id: string) {
-        return `This action returns a #${id} article`;
+        return this.prisma.article.findUnique({
+            where: { id },
+            include: { category: true },
+        });
     }
 
     update(id: string, updateArticleDto: UpdateArticleDto) {
-        return `This action updates a #${id} article`;
+        return this.prisma.article.update({
+            where: { id },
+            data: updateArticleDto,
+        });
     }
 
     remove(id: string) {
-        return `This action removes a #${id} article`;
+        return this.prisma.article.delete({
+            where: { id },
+        });
     }
 }
